e632b9845b02979339b8229c84036cdf93f8f3f3
Yehor Lapko <yehorlapko@zoho.com>
2019-10-17 06:18:40 +0800

    bla

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -10,8 +10,7 @@ export default class ImagesList extends Component {
     };
 
     componentDidMount() {
-        const apiKey = 'aa0eea0493a6428e612c5e4569f5e8008995c7d3753ee0d64a0e1e67a38a2d42'
-        axios.get(`https://api.unsplash.com/search/photos?client_id=${apiKey}&page=1&query=cake&per_page=15`).then(res => {
+        axios.get(`https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ID}&page=1&query=cake&per_page=15`).then(res => {
             console.log(res.data.results);
             this.setState({
                 images: res.data.results
